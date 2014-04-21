44a7c0681262af775a3f5fcaf0be9a677feca752
dspk <dspk@users.noreply.github.com>
2014-04-21 15:03:39 -0700

    Update LogisticRegression.R

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -28,27 +28,27 @@ gradient = function(theta.initial, X, y, m){
 #. Try out the logistic model with a dataset
 getwd()
 setwd("SET CORRECT WORKING DIRECTORY")
-ex1 = read.table("./ex2data1.txt", header=FALSE, sep=",")
-colnames(ex1) = c("exam1score", "exam2score", "admission")
-m = nrow(ex1) # number of training examples
+Usedat = read.table("./Usedat.txt", header=FALSE, sep=",")
+colnames(Usedat) = c("xvar1", "xvar2", "yvaradmit")
+m = nrow(Usedat) # number of training examples
 
 #. Construct matrices X and y
 x0 = rep(1,m)
-X.matrix = as.matrix(cbind(x0, ex1[, -3]))
-y.matrix = as.matrix(ex1[, 3])
+X.matrix = as.matrix(cbind(x0, Usedat[, -3]))
+y.matrix = as.matrix(Usedat[, 3])
 
 #. Summarize and plot data
-summary(ex1)
+summary(Usedat)
 install.packages("ggplot2")
 library(ggplot2)
-qplot(x = exam1score, y =exam2score, data=ex1, colour=factor(admission)) +
-  geom_point(aes(shape=factor(admission)), size=3) +
+qplot(x = xvar1, y =xvar2, data=Usedat, colour=factor(yvaradmit)) +
+  geom_point(aes(shape=factor(yvaradmit)), size=3) +
   labs(title = "Student exam scores", colour = "admit", shape="admit")
 
 #. Plot sigmoid function
 def.newdata = data.frame(y = c(rep(0, 50), rep(1, 50)), x = seq(-4, 4, length.out=100) )
 qplot(x = x, y = y, data=def.newdata) +
-  geom_point() + 
+  geom_point() +
   stat_function(fun = sigmoid) +
   labs(title = "Sigmoid function")
 
@@ -56,6 +56,6 @@ qplot(x = x, y = y, data=def.newdata) +
 theta.initial = matrix(0, nrow=dim(X.matrix)[2], ncol=1)
 
 #. Use an optimization solver that finds the minimum of an unconstrained function
-#. In our case we want to find the parameters theta that minimize the cost function 
+#. In our case we want to find the parameters theta that minimize the cost function
 theta_optimal = optim(theta.initial, cost, gradient, X=X.matrix, y=y.matrix, m=nrow(X.matrix))
-print(theta_optimal)  #. final theta values
+print(theta_optimal) #. final theta values
